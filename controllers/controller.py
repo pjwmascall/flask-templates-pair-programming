e096b40609aa3f911641fbfebe63732a91d28f46
James <jamesg487@gmail.com>
2022-01-27 14:59:47 +0000

    added templates and form capabilities

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from models.events_list import events
+from models.events_list import events, add_new_event
 from models.events import Event
 
 @app.route('/')
@@ -9,11 +9,11 @@ def index():
 
 @app.route('/', methods=['POST'])
 def add_event():
-    date = form.request[date]
-    name = form.request[name]
-    number_of_guests = form.requests[number_of_guests]
-    room_location = form.requests[room_location]
-    description = form.requests[description]
+    date = request.form["date"]
+    name = request.form["name"]
+    number_of_guests = request.form["number_of_guests"]
+    room_location = request.form["room_location"]
+    description = request.form["description"]
     new_event = Event(date, name, number_of_guests, room_location, description)
     add_new_event(new_event)
     return render_template('index.html', events=events)
